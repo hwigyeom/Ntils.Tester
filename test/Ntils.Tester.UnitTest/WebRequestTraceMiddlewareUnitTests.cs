3ecb2080624d68c5095718a567b46a83362ed4c5
노휘겸 <whistle96@gmail.com>
2017-11-01 00:40:40 +0900

    유닛 테스트 프로젝트 추가 및 경로 변경

---
+++ test/Ntils.Tester.UnitTest/WebRequestTraceMiddlewareUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ntils.Tester.UnitTest
+{
+    public class WebRequestTraceMiddlewareUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
